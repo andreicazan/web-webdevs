77f856ce1ab40f1993294a7e00ce243d871ec19c
andreicazan <andreicazan11@yahoo.com>
2021-03-27 21:53:49 +0200

    connect with firebase

---
+++ WebApp/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ WebApp/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAIZ1xF2BFW1WxCtJgP300IZJMWZFooD4s",
+    authDomain: "tripapp-adaea.firebaseapp.com",
+    projectId: "tripapp-adaea",
+    storageBucket: "tripapp-adaea.appspot.com",
+    messagingSenderId: "622867006156",
+    appId: "1:622867006156:web:80842f198a120257b97869",
+    measurementId: "G-SRDHWJZG7C"
+  }
+};
